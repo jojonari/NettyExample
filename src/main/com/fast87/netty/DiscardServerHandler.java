12d9d1b0a82bd69ea8b25a26ed8639939f2a5d0a
jojonari <xkgun@naver.com>
2017-11-06 14:07:49 +0900

    자바 네트워크 소녀 Netty 1장

---
+++ src/main/com/fast87/netty/DiscardServerHandler.java
@@ -0,0 +1,19 @@
+package com.fast87.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
+
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		cause.printStackTrace();
+		ctx.close();
+	}
+	
+}
